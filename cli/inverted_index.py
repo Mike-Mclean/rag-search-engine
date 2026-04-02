666db9d149935f061b1b52698b52de378560875b
Mike-Mclean <mmcle009@gmail.com>
2026-04-02 13:29:09 -0400

    Added inverted index

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,38 +1,10 @@
-from query_process import *
-import pickle
-import os
 
 class InvertedIndex:
-    def __init__(self, index = {}, docmap = {}):
+    def __init__(self, index: dict, docmap: dict):
         self.index = index
         self.docmap = docmap
 
     def __add_document(self, doc_id, text):
-        tokenized_text = preprocess_text(text)
-        for token in tokenized_text:
-            if token not in self.index:
-                self.index[token] = set()
-            self.index[token].add(doc_id)
+       pass
 
-    def get_documents(self, term):
-        porcessed_term = term.lower()
-        return sorted(list(self.index[porcessed_term]))
-
-    def build(self):
-        movies_collection = load_movies(DATA_PATH)
-        for movie in movies_collection:
-            id = movie["id"]
-            self.docmap[id] = movie["description"]
-            self.__add_document(id, (f"{movie["title"]} {movie["description"]}"))
-
-    def save(self):
-        CACHE_PATH = os.path.join(ROOT_PATH, "cache")
-        os.makedirs(CACHE_PATH, exist_ok=True)
-        index_path = os.path.join(CACHE_PATH, "index.pkl")
-        docmap_path = os.path.join(CACHE_PATH, "docmap.pkl")
-
-        with open(index_path, 'wb') as index_file:
-            pickle.dump(self.index, index_file)
-
-        with open(docmap_path, 'wb') as docmap_file:
-            pickle.dump(self.docmap, docmap_file)
+    
--- cli/query_process.py
+++ cli/query_process.py
@@ -1,36 +1,5 @@
 import string
 from nltk.stem import PorterStemmer
-import os
-import json
-
-ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
-DATA_PATH = os.path.join(ROOT_PATH, "data", "movies.json")
-SEARCH_LIMIT = 5
-
-def load_movies(path):
-    with open(path, "r") as json_file:
-        data = json.load(json_file)
-    return data["movies"]
-
-def compare_tokens(query_tokens, movie_tokens):
-    for query_token in query_tokens:
-        for movie_token in movie_tokens:
-            if query_token in movie_token:
-                return True
-    return False
-
-def search_command(query, limit = SEARCH_LIMIT):
-    movies = load_movies(DATA_PATH)
-
-    results = []
-    preprocessed_query = preprocess_text(query)
-    for movie in movies:
-        preprocessed_title = preprocess_text(movie["title"])
-        if (compare_tokens(preprocessed_query, preprocessed_title)):
-            results.append(movie['title'])
-        if len(results) >= limit:
-            break
-    return results
 
 def preprocess_text(text: str) -> str:
 
